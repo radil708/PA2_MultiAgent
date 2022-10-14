3c095cdd1ba9a14d4883df03c69cd3b8946c1c94
radil <adil.r@northeastern.edu>
2022-10-14 16:29:26 -0400

    Rewrote alpha beta pruning

--- multiAgents.py
+++ multiAgents.py
@@ -324,6 +324,150 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
+        #Rewrite to fix bug
+
+        #initialize variables
+        start_node_value = float('-inf')
+        best_action_from_start = Directions.STOP
+        start_alpha = float('-inf')
+        start_beta = float('inf')
+        initial_depth = 0
+
+        #need this var for the for loop to compare and find max score
+        indiv_state_calc_value = float('-inf')
+
+        # we start at pacman current state
+        # let's check if the game is over
+        if gameState.isWin() or gameState.isLose():
+            # if the game is over we still need to return an action
+            return best_action_from_start
+
+        #now we need to get all actions from start and find the best score among them
+        #pacman index is 0 so this is all possible actions from current state of pacman
+        all_actions_from_start_node = gameState.getLegalActions(0)
+
+        # generate all states of pacman as a result of an action
+        future_states_list = []
+
+        for each_action in all_actions_from_start_node:
+            # get the new state as the result of a pacman action
+            future_states_list.append(gameState.generateSuccessor(0, each_action))
+            # this state is incomplete since it doesn't take into account the actions of the ghost agents
+
+        #lets get the score calculated from the min node
+
+        for each_transition_state_index in range(len(future_states_list)):
+            indiv_state_calc_value = get_minimizer_score()
+
+            #since start is a maximizer node we need to get a value that is the largest
+            if indiv_state_calc_value > start_node_value:
+                start_node_value = indiv_state_calc_value
+                #update the best action to return
+                best_action_from_start = all_actions_from_start_node[each_transition_state_index]
+
+        return best_action_from_start
+
+        def get_best_minimizer_score_from_minimizer_level(self, gameState: GameState, current_depth: int, min_alpha_v,min_beta_v):
+            # first check if the game is over or if we have reached the terminal depth/node
+            if gameState.isWin() or gameState.isLose() or current_depth == self.depth:
+                return self.evaluationFunction(gameState)
+
+            #initialize variables to keep track of things
+            lowest_minimizer_value = float('inf')
+            saved_prev_val = None
+            current_calc_minimizer_score = float('inf')
+
+            # lets get all the states resulting from all ghost agents moving
+            # This will complete one state
+            total_ghost_agents = gameState.getNumAgents() - 1 #total_agents = pacman + ghost 1 + ghost 2 + etc.. ghost x
+
+
+            for i in range(0, total_ghost_agents):
+                pruned_flag = False
+                current_ghost_agent_index = i + 1
+
+                all_future_possible_actions_of_ghost = gameState.getLegalActions(current_ghost_agent_index)
+
+                # each action leads to one state
+                # for each state get the score
+                for each_action in all_future_possible_actions_of_ghost:
+                    future_state = gameState.generateSuccessor(current_ghost_agent_index, each_action)
+                    current_calc_minimizer_score = get_max_score()
+
+                    lowest_minimizer_value = min(lowest_minimizer_value,current_calc_minimizer_score)
+                    min_beta_v = min(min_beta_v, lowest_minimizer_value)
+
+                    #pruning happens here
+                    # means that lowest value we can get is already less than the best value
+                    # for the maximizer above us, cut this off because maximizer will ignore this node already
+                    # go check out next available node
+                    if min_beta_v <= min_alpha_v:
+                        pruned_flag = True
+                        break
+
+                    if pruned_flag == False:
+                        saved_prev_val = current_calc_minimizer_score
+
+            if pruned_flag == True:
+                return saved_prev_val
+            else:
+                return lowest_minimizer_value
+
+        def get_best_maximizer_score_from_maximizer_level(self, gameState: GameState, current_depth: int, max_alpha_v,max_beta_v):
+            # first check if the game is over or if we have reached the terminal depth/node
+            if gameState.isWin() or gameState.isLose() or current_depth == self.depth:
+                return self.evaluationFunction(gameState)
+
+            largest_maximizer_value = float('-inf')
+            current_calc_maximizer_value = float('-inf')
+            saved_prev_val = None
+
+            #get all possible actions of maximizer agent/pacman
+            all_potential_actions = gameState.getLegalActions(0)
+
+            #one action can generate one state
+            # check the best score from each state
+
+
+            for each_action in all_potential_actions:
+
+                pruned_flag = False
+
+                future_state = gameState.generateSuccessor(0,each_action)
+
+                #score of current state
+                current_calc_maximizer_value = \
+                    get_best_minimizer_score_from_minimizer_level(
+                        self,future_state, current_depth,max_alpha_v,max_beta_v)
+
+                largest_maximizer_value = max(largest_maximizer_value, current_calc_maximizer_value)
+                max_alpha_v = max(max_alpha_v, largest_maximizer_value)
+
+                #This means that the value of this node is alread larger than the minimizer node value above it
+                # minimizer node will ignore this node so lets prune
+                if max_alpha_v > max_beta_v:
+                    pruned_flag = True
+                    break
+
+                if pruned_flag == False:
+                    saved_prev_val = largest_maximizer_value
+
+            if pruned_flag == True:
+                return saved_prev_val
+            else:
+                return largest_maximizer_value
+
+
+
+
+
+
+
+
+        def maximizer_node_get_score(self, gameState: GameState, current_depth: int, alpha_v,beta_v):
+
+
+
 
         def max_agent(self, gameState: GameState, current_depth: int, alpha_v,beta_v):
             # first check if the game is over or if we have reached the terminal depth/node
